15e3e2e183e70c83286c422cf804ac94d69a61fd
AbimanyuGGWP <abimanyu.tugas@gmail.com>
2021-10-04 10:50:03 +0700

    first commit

---
+++ .vscode/print.py
@@ -0,0 +1,6 @@
+print ("Masukkan Bilangan : ")
+bilangan = int(input())
+if bilangan % 2 == 0:
+    print(str(bilangan) + " adalah bilangan genap ")
+else:
+    print(str(bilangan) + " adalah bilangan ganjil ")
